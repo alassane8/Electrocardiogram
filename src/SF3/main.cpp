d92b03aec3b8c6ae24deb498286d7c31232c0ca3
alassane8 <alassane.wade@edu.ece.fr>
2023-12-22 12:43:22 +0100

    feat: update main.cpp SF3

--- src/SF3/main.cpp
+++ src/SF3/main.cpp
@@ -4,6 +4,7 @@
 const int red_Led = 2;
 const int green_Led = 3;
 const int yellow_Led = 4;
+int heart_rate;
 
 //Fonctions setup
 void setup(){
@@ -12,16 +13,22 @@ void setup(){
     pinMode(yellow_Led, OUTPUT);
 }
 void loop(){
-    digitalWrite(red_Led, HIGH);
-    delay(500);
-    digitalWrite(red_Led, LOW);
-    delay(500);
-    digitalWrite(green_Led, HIGH);
-    delay(500);
-    digitalWrite(green_Led, LOW);
-    delay(500);
-    digitalWrite(yellow_Led, HIGH);
-    delay(500);
-    digitalWrite(yellow_Led, LOW);
-    delay(500);
+    while (heart_rate >= 150){
+        digitalWrite(red_Led, HIGH);
+        delay(500);
+        digitalWrite(red_Led, LOW);
+        delay(500);
+    }
+    while (heart_rate < 150 && heart_rate > 50){
+        digitalWrite(green_Led, HIGH);
+        delay(500);
+        digitalWrite(green_Led, LOW);
+        delay(500);
+    }
+    while (heart_rate < 50){
+        digitalWrite(yellow_Led, HIGH);
+        delay(500);
+        digitalWrite(yellow_Led, LOW);
+        delay(500);
+    }
 }
