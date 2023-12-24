58605b271881098df7b72656a38b04e231b10606
alassane8 <alassane.wade@edu.ece.fr>
2023-12-24 11:21:34 +0100

    feat: comments on main.cpp

--- src/SF3/main.cpp
+++ src/SF3/main.cpp
@@ -1,31 +1,39 @@
 #include <Arduino.h>
 
-//Declarations des variables
+//Variables definitions
 const int red_Led = 2;
 const int green_Led = 3;
 const int yellow_Led = 4;
 int heart_rate;
-//Fonctions setup
+
+//Functions setup
 void setup(){
     pinMode(red_Led, OUTPUT);
     pinMode(green_Led, OUTPUT);
     pinMode(yellow_Led, OUTPUT);
 }
+
+//Functions loop
 void loop(){
 
+    //Heart rate High
     while (heart_rate >= 150){
         digitalWrite(red_Led, HIGH);
         delay(500);
         digitalWrite(red_Led, LOW);
         delay(500);
     }
+
+    //Heart rate Normal
     while (heart_rate < 150 && heart_rate > 50){
         digitalWrite(green_Led, HIGH);
         delay(500);
         digitalWrite(green_Led, LOW);
         delay(500);
     }
-    while (heart_rate < 50){
+
+    //Heart rate Low
+    while (heart_rate <= 50){
         digitalWrite(yellow_Led, HIGH);
         delay(500);
         digitalWrite(yellow_Led, LOW);
